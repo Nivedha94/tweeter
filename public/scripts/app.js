b29af159921a19c175f306daed9162b203326b55
vagrant <nivedha.selvan@gmail.com>
2021-11-21 18:09:23 +0000

    Created function loadTweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,31 +7,7 @@
 */
 
  // Test / driver code (temporary). Eventually will get this from the server.
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+
 
 /*
 <article class="tweet">
@@ -146,4 +122,10 @@ const renderTweets = function(tweets) {
   }
 };
 
-renderTweets(data);
+const loadTweets = () => {
+  $.get('/tweets')
+    .then(tweets => renderTweets(tweets))
+    .fail(error => console.error(error));
+};
+
+loadTweets();
